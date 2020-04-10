e3d894eb2e969a2bd46b8fd4a83e22cb8f35b388
sumedh105 <sumedh105@gmail.com>
2020-04-10 23:12:55 +0200

    Program to take inputs and output

---
+++ GeeksForGeeks/Basics/inputOutput.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hello", name)
+
+num1 = int(input("Enter a number: "))
+print("The number is:", num1)
+
+num2 = int(input("Enter a number: "))
+print("The number is:", num2)
+
+num3 = num1 * num2
+print(num3)
