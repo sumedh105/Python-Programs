2c11fc6c3f7293cd6100fef133f38c62fc205e25
sumedh105 <sumedh105@gmail.com>
2020-04-10 23:18:09 +0200

    Program to implement the function with main function

---
+++ GeeksForGeeks/Basics/functionWithMain.py
@@ -0,0 +1,10 @@
+def getInteger():
+    result = int(input("Enter the integer: "))
+    return result
+
+def MAIN():
+    print("I am in the main function")
+    print("The result is: ", getInteger())
+
+if __name__ == "__main__":
+    MAIN()
