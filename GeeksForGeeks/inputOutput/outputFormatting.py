d438c5f226c1ec17f350c3e7ea047f237383741c
sumedh105 <sumedh105@gmail.com>
2020-04-11 22:01:48 +0200

    Program to implement the output formatting using the format function

---
+++ GeeksForGeeks/inputOutput/outputFormatting.py
@@ -0,0 +1,11 @@
+print("I love {} and {}".format("Geeks", "Geeks"))
+print("{0} and {1}".format("Geeks", "Portal"))
+print("{1} and {0}".format("Geeks", "Portal"))
+
+# Combining positional and keyword arguments.
+print("Number one portal is: {0} {1} {other}".format("Geeks", "for", other="Geeks"))
+
+# Using format method with number
+print("Geeks: {0:2d}, Portal: {1:8.2f}".format(12, 00.546))
+
+
