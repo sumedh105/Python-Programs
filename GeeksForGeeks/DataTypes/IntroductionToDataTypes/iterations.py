01781b222d0bf7027a84f0e3d8f453b24bbc2199
sumedh105 <sumedh105@gmail.com>
2020-04-12 00:23:01 +0200

    Program to implement the iterators

---
+++ GeeksForGeeks/DataTypes/IntroductionToDataTypes/iterations.py
@@ -0,0 +1,19 @@
+# Iteration by while loop
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+
+# Iteration by for loop on string
+string = "Sumedh W. Jambekar from Karnataka, India"
+for index in string:
+    print(index)
+
+# Iteration by for loop on list
+List = ["Sumedh W. Jambekar", "Kalaburagi", "Karnataka", "India"]
+for index in List:
+    print(index)
+
+# Iteration by for loop for range
+for index in range(0, 10):
+    print(index)
