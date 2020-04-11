69f6780b502703cd743c6e0320ce4ef4776d42db
sumedh105 <sumedh105@gmail.com>
2020-04-12 00:14:51 +0200

    Program to implement a list in python

---
+++ GeeksForGeeks/DataTypes/IntroductionToDataTypes/list.py
@@ -0,0 +1,28 @@
+# Declaring a list
+L = ["Sumedh W. Jambekar", "16-03-1987", 585103, 10697]
+
+# Print the contents of the list
+print("The list is: ", L)
+
+# Append an element to a list
+L.append("Kalaburagi")
+print("The list is: ", L)
+
+# Append an element to a list
+L.append("Karnataka")
+print("The list is: ", L)
+
+# Append an element to a list
+L.append("India")
+print("The list is: ", L)
+
+# Pop an element from a list
+L.pop()
+print("The list is: ", L)
+
+# Print the contents of the list by index
+print(L[2])
+
+for index in range(len(L)):
+    print(L[index])
+
