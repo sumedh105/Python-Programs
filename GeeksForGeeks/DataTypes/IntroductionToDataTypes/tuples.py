9067b6361214d3818b0d13652a7e4398284ff3b9
sumedh105 <sumedh105@gmail.com>
2020-04-12 00:17:55 +0200

    Program to implement a tuple

---
+++ GeeksForGeeks/DataTypes/IntroductionToDataTypes/tuples.py
@@ -0,0 +1,7 @@
+# Define a tuple
+tup = ("Sumedh W. Jambekar", "Kalaburagi", "Karnataka", "India")
+print(tup)
+
+# Print the contents of a tuple
+for index in range(len(tup)):
+    print(tup[index])
