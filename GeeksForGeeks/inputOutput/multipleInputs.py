65f63f38952953cf7c45b1ceb5be70c7aa30bee2
sumedh105 <sumedh105@gmail.com>
2020-04-11 20:02:41 +0200

    Program to implement taking multiple inputs

---
+++ GeeksForGeeks/inputOutput/multipleInputs.py
@@ -0,0 +1,16 @@
+# Taking two inputs at a time
+x, y = input("Enter two numbers: ").split()
+
+print("Number of boys are: ", str(x));
+print("Number of girsl are: ", str(y))
+
+# Taking 3 inputs at a time
+x, y, z = input("Enter three numbers: ").split()
+
+print("Number of students are: ", str(x))
+print("Total number of boys are: ", str(y))
+print("To number of girls are: ", str(z))
+
+a, b = input("Enter two numbers: ").split()
+print("The two numbers are {} and {}".format(a, b));
+
