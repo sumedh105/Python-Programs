63a52252722cbc8928e5ad134b39b75ae69e5e9b
sumedh105 <sumedh105@gmail.com>
2020-04-11 21:45:09 +0200

    Program to implmenet the usage of sep

---
+++ GeeksForGeeks/inputOutput/sepParameterInPrint.py
@@ -0,0 +1,4 @@
+print("Hello", "Sumedh W. Jambekar", "is from", "Gulbarga", "Karnataka", "India", sep = "-")
+
+print("16", "03", "1987", sep = "/", end = " ")
+print("is the birthday of Sumedh W. Jambekar")
