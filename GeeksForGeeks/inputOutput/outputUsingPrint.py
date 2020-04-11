3e08f89b15a5349e9c0ea5749e3b42c2954eb9a7
sumedh105 <sumedh105@gmail.com>
2020-04-11 20:11:14 +0200

    Program to implement the muliple outputs using print

---
+++ GeeksForGeeks/inputOutput/outputUsingPrint.py
@@ -0,0 +1,13 @@
+# One object is passed
+print("GeeksForGeeks")
+
+# Two objects are passed
+x = 5
+print("x = ", x)
+
+# Code for using the 'sep' feature
+print('G', 'F', 'H', sep = ' ')
+
+# Code using the 'end' keyword
+print("Hello ", end = "@ ")
+print("Sumedh W. Jambekar")
