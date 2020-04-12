fc935682c6235663282ba2aca42f88d96504b336
sumedh105 <sumedh105@gmail.com>
2020-04-12 13:45:43 +0200

    Program to add elements to a list

---
+++ GeeksForGeeks/DataTypes/List/addingElementsToList.py
@@ -0,0 +1,20 @@
+# Create an empty list
+List = []
+print(List)
+
+# Adding an integer to a list
+List.append(10)
+print(List)
+
+# Adding a string to a list
+List.append("Sumedh")
+print(List)
+
+# Adding a tuple to a list
+List.append((10, 20))
+print(List)
+
+# Adding a list to a list
+ListTwo = ["Sumedh", "W.", "Jambekar"]
+List.append(ListTwo)
+print(List)
