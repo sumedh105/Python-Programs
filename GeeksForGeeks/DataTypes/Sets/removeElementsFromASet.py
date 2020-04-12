265879053b97e575cb771fdc51a06f8065cfd142
sumedh105 <sumedh105@gmail.com>
2020-04-12 19:34:32 +0200

    Program to remove the elemensts from a set

---
+++ GeeksForGeeks/DataTypes/Sets/removeElementsFromASet.py
@@ -0,0 +1,11 @@
+# Create a set
+setOne = set([16, 3, 1987, "Sumedh", "W.", "Jambekar"])
+print(setOne)
+
+# Removing an element using discard() method
+setOne.discard("Sumedh")
+print(setOne)
+
+# Removing an element using discard() method
+setOne.discard(1987)
+print(setOne)
