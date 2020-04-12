beae5748cadd6fff18d9e2da9dd424972d08e2e6
sumedh105 <sumedh105@gmail.com>
2020-04-12 13:37:13 +0200

    Program to create a list

---
+++ GeeksForGeeks/DataTypes/List/createList.py
@@ -0,0 +1,38 @@
+# Blank list
+List = []
+print("Blank list")
+print(List)
+
+# List of numbers
+List.append(10)
+List.append(20)
+List.append(30)
+print(List)
+
+# Pop list
+List.pop()
+List.pop()
+List.pop()
+print(List)
+
+# List of strings
+List.append("Sumedh")
+List.append("W.")
+List.append("Jambekar")
+print(List)
+
+# Accessing the list
+print(List[0])
+print(List[1])
+print(List[2])
+
+# Pop list
+List.pop()
+List.pop()
+List.pop()
+print(List)
+
+# Creating a list of mixed data types
+List.append(10)
+List.append("Sumedh W. Jambekar")
+print(List)
