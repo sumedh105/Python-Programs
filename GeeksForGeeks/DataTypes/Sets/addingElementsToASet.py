af1c6b365c19c945e24b2dfbaf4988429515125a
sumedh105 <sumedh105@gmail.com>
2020-04-12 19:21:06 +0200

    Program to add elements to a set

---
+++ GeeksForGeeks/DataTypes/Sets/addingElementsToASet.py
@@ -0,0 +1,13 @@
+# Create an empty set
+setOne = set()
+print(setOne)
+
+# Add elements to a set
+setOne.add(16)
+setOne.add(3)
+setOne.add(1987)
+setOne.add("Sumedh")
+setOne.add("W.")
+setOne.add("Jambekar")
+
+print(setOne)
