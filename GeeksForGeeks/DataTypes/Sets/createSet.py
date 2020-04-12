917554c8434d7a6d4ef89548722315d858551f87
sumedh105 <sumedh105@gmail.com>
2020-04-12 19:17:58 +0200

    Progrma to create a set

---
+++ GeeksForGeeks/DataTypes/Sets/createSet.py
@@ -0,0 +1,24 @@
+# Create an empty set
+setOne = set()
+print(setOne)
+
+# Create a set with integers in it
+setTwo = set([10, 20])
+print(setTwo)
+
+# Create a set with string in it
+setThree = set("Sumedh W. Jambekar")
+print(setThree)
+
+# Create a set using curly braces
+setFour = {"Sumedh", "W.", "Jambekar"}
+print(setFour)
+
+# Create a set by passing an object
+string = "Sumedh W. Jambekar"
+setFive = set(string)
+print(setFive)
+
+# Create a set with mixed data types in it
+setSix = set([16, 3, 1987, "Sumedh", "W.", "Jambekar"])
+print(setSix)
