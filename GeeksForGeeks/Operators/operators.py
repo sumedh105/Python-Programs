7f8a5624d8568dd77c404ada8c6a84fc47154d32
sumedh105 <sumedh105@gmail.com>
2020-04-12 23:42:05 +0200

    Program to use different operators in Pthon

---
+++ GeeksForGeeks/Operators/operators.py
@@ -0,0 +1,52 @@
+a = 10
+b = 20
+
+# + operator
+print(a + b)
+
+# - operator
+print(a - b)
+
+# * operator
+print(a * b)
+
+# / operator
+print(a / b)
+
+# // operator
+print(b // a)
+
+# % operator
+print(a % b)
+
+# ** operator
+print(a ** b)
+
+# > operator
+print(a > b)
+
+# < operator
+print(a < b)
+
+# == operator
+print(a == b)
+
+# != operator
+print(a != b)
+
+# <= operator
+print(a <= b)
+
+# >= operator
+print(a >= b)
+
+# and operator
+print(a and b)
+
+# or operator
+print(a or b)
+
+# in operator
+stringOne = "Sumedh W. Jambekar"
+print('S' in stringOne)
+print('T' not in stringOne)
