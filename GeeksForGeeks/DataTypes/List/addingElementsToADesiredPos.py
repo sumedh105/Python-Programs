d1977b773430f7b37648759a7c66ccc899aef237
sumedh105 <sumedh105@gmail.com>
2020-04-12 14:40:15 +0200

    Program to use insert() function

---
+++ GeeksForGeeks/DataTypes/List/addingElementsToADesiredPos.py
@@ -0,0 +1,15 @@
+# Create a list
+ListOne = ["Sumedh", "W.", "Jambekar"]
+print(ListOne)
+
+# Adding a number at a specific position in a list
+ListOne.insert(3, 16)
+print(ListOne)
+
+# Adding a number at a specific position in a list
+ListOne.insert(4, 3)
+print(ListOne)
+
+# Adding a number at a specific position in a list
+ListOne.insert(5, 1987)
+print(ListOne)
