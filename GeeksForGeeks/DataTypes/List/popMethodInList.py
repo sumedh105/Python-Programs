23b87ff08e883a0792ec73958cfb11947daed401
sumedh105 <sumedh105@gmail.com>
2020-04-12 14:55:55 +0200

    Program to use the pop method in the list

---
+++ GeeksForGeeks/DataTypes/List/popMethodInList.py
@@ -0,0 +1,11 @@
+# Create a list
+List = ["Sumedh", "W.", "Jambekar", 16, 3, 1987]
+print(List)
+
+# Using the pop() to remove an element from the list
+List.pop()
+print(List)
+
+# Using the pop() to remove a specific element from the list
+List.pop(2)
+print(List)
