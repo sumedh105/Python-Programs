74f95769c2bf29204ebe8516386ae52c573b0347
sumedh105 <sumedh105@gmail.com>
2020-04-12 19:26:41 +0200

    Program to use the update method in set

---
+++ GeeksForGeeks/DataTypes/Sets/updateMethodSet.py
@@ -0,0 +1,14 @@
+setOne = set()
+print(setOne)
+
+# Add elements to set using add() function
+setOne.add(16)
+setOne.add(3)
+setOne.add(1987)
+print(setOne)
+
+# Updating the set using update() function
+setOne.update("Sumedh")
+setOne.update(["W.", "Jambekar"])
+print(setOne)
+
