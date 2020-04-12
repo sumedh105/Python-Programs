3c1e1d62ca56309c02705517929ea8178be1af89
sumedh105 <sumedh105@gmail.com>
2020-04-12 12:33:07 +0200

    Program to implement the escape sequences

---
+++ GeeksForGeeks/DataTypes/Strings/escapeSequence.py
@@ -0,0 +1,15 @@
+# Initial string
+stringOne = '''I'm a "Geek"'''
+print(stringOne)
+
+# Escaping single quote
+stringOne = 'I\'m a "Geek"'
+print(stringOne)
+
+# Escaping double quotes
+stringOne = "I'm a \"Geek\""
+print(stringOne)
+
+# Printing paths with the escape sequences
+stringOne = "C:\Python\Geeks"
+print(stringOne)
