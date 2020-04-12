4b4ed5d1fe7bc45bc7437c4d7377d9fe7731198b
sumedh105 <sumedh105@gmail.com>
2020-04-12 22:24:06 +0200

    Adding elemetns to a dictionary

---
+++ GeeksForGeeks/DataTypes/Dictionary/addElementsToDictionary.py
@@ -0,0 +1,16 @@
+dictOne = {}
+print(dictOne)
+
+# Adding elements one at a time to dictionary
+dictOne[0] = "Sumedh"
+dictOne[1] = "W."
+dictOne[2] = "Jambekar"
+print(dictOne)
+
+# Adding a value set to a dictionary
+dictOne["ValueSet"] = "Kalaburagi", "Karnataka", "India"
+print(dictOne)
+
+# Updating the existing value in a dictionary
+dictOne[1] = "WB"
+print(dictOne)
