62b4cc5f909e4161c485536c3778fc517dfa8808
sumedh105 <sumedh105@gmail.com>
2020-04-12 15:00:40 +0200

    Program to slice the list

---
+++ GeeksForGeeks/DataTypes/List/listSlicing.py
@@ -0,0 +1,12 @@
+# Create a list
+List = ["Sumedh", "W.", "Jambekar", 16, 3, 1987]
+print(List)
+
+# Print the whole list using the slice operator
+print(List[:])
+
+# Print the whole list using the slice operator and using the negative index
+print(List[::-1])
+
+# Print the whole list using the slice operator and using the negative index
+print(List[-5:-3])
