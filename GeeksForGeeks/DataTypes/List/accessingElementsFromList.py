0e1fb5c0546cb625fa55c084f6e98c3a2821f351
sumedh105 <sumedh105@gmail.com>
2020-04-12 14:47:30 +0200

    Program to access the elements from the list

---
+++ GeeksForGeeks/DataTypes/List/accessingElementsFromList.py
@@ -0,0 +1,25 @@
+# Creating a list
+List = ["Sumedh", "W.", "Jambekar", 16, 3, 1987]
+print(List)
+
+# Appending a tuple to the existing list
+List.append((10, 20))
+print(List)
+
+# Accessing the list
+print(List[0])
+print(List[0][0])
+print(List[0][1])
+print(List[0][2])
+print(List[0][3])
+print(List[0][4])
+print(List[0][5])
+print(List[1])
+print(List[2])
+print(List[3])
+print(List[4])
+print(List[5])
+print(List[6])
+print(List[6][0])
+print(List[6][1])
+
