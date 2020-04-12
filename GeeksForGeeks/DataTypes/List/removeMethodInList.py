267ad3fc3c03f6f55560c2f6b559cc42fa752312
sumedh105 <sumedh105@gmail.com>
2020-04-12 14:53:26 +0200

    Program to use the remove method

---
+++ GeeksForGeeks/DataTypes/List/removeMethodInList.py
@@ -0,0 +1,11 @@
+# Create a list
+List = ["Sumedh", "W.", "Jambekar", 16, 3, 1987]
+print(List)
+
+# Remove a specfic item in the list
+List.remove("Sumedh")
+print(List)
+
+# Remove a specfic item in the list
+List.remove(1987)
+print(List)
