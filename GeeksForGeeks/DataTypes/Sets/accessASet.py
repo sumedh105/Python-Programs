ef9a20c4861cb7dfc1288c1df044d0286403a467
sumedh105 <sumedh105@gmail.com>
2020-04-12 19:29:52 +0200

    Program to access a set

---
+++ GeeksForGeeks/DataTypes/Sets/accessASet.py
@@ -0,0 +1,18 @@
+# Create an empty set
+setOne = set()
+print(setOne)
+
+# Add elements to a set
+setOne.add(16)
+setOne.add(3)
+setOne.add(1987)
+setOne.add("Sumedh")
+setOne.add("W.")
+setOne.add("Jambekar")
+print(setOne)
+
+# Accessing the set using iterators
+for index in setOne:
+    print(index, end = "")
+
+print()
