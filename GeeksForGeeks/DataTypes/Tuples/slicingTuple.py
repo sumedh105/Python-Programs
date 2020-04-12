4f1ec6194e8c55793a6c639982a967c2f3255295
sumedh105 <sumedh105@gmail.com>
2020-04-12 18:41:17 +0200

    Program to slice the tuple

---
+++ GeeksForGeeks/DataTypes/Tuples/slicingTuple.py
@@ -0,0 +1,11 @@
+TupleOne = ("Sumedh W. Jambekar", "Kalaburagi", "Karnataka", "India")
+print(TupleOne)
+
+# Slicing the tuple, printing the entire tuple
+print(TupleOne[:])
+
+# Slicing the tuple, printing the subset of tuple
+print(TupleOne[0:2])
+
+# Slicing the tuple, printing the subset of tuple
+print(TupleOne[0:-1])
