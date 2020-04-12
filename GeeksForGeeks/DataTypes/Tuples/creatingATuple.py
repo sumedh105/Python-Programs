1b5d4aa6d24b44da5c3449f8d058df3bce0c36cd
sumedh105 <sumedh105@gmail.com>
2020-04-12 18:36:49 +0200

    Program to create a tuple

---
+++ GeeksForGeeks/DataTypes/Tuples/creatingATuple.py
@@ -0,0 +1,32 @@
+# Create a tuple of strings
+TupleOne = ("Sumedh" , "W.", "Jambekar")
+print(TupleOne)
+
+# Create a tuple of integers
+TupleTwo = (10, 20, 30, 40)
+print(TupleTwo)
+
+# Create a mixed tuple of integers and strings
+TupleThree = ("Sumedh", "W.", "Jambekar", 16, 3, 1987)
+print(TupleThree)
+
+# Create a tuple using a list and using a tuple() function
+List = ["Sumedh", "W.", "Jambekar", 16, 3, 1987]
+TupleFour = tuple(List)
+print(TupleFour)
+
+# Create a tuple using the tuple() function
+TupleFive = tuple("Sumedh")
+print(TupleFive)
+
+# Creating a tuple with nested tuples.
+TupleSix = (10, 20)
+TupleSeven = ("Sumedh W. Jambekar")
+TupleEight = (TupleSix, TupleSeven)
+print(TupleEight)
+
+# Creating a tuple with repetition
+TupleNine = "Sumedh W. Jambekar," * 3
+print(TupleNine)
+
+
