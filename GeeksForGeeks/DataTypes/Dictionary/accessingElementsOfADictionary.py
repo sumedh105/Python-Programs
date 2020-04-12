089655d4bcf788588b42f60ccef86c5fff152ab2
sumedh105 <sumedh105@gmail.com>
2020-04-12 22:28:39 +0200

    Program to access elemetns of a dictionary

---
+++ GeeksForGeeks/DataTypes/Dictionary/accessingElementsOfADictionary.py
@@ -0,0 +1,13 @@
+# Creating a dictionary
+dictOne = {"Name":"Sumedh W. Jambekar", "DoB":"16-03-1987", "Country":"India"}
+print(dictOne)
+
+# Accessing elements of a dictionary
+print(dictOne["Name"])
+print(dictOne["DoB"])
+print(dictOne["Country"])
+
+# Using get() to access elements from a dictionary
+print(dictOne.get("Name"))
+print(dictOne.get("DoB"))
+print(dictOne.get("Country"))
