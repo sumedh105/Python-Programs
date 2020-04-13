b8097be21f4d493349a8cf319b5dbcef18b4d967
sumedh105 <sumedh105@gmail.com>
2020-04-13 11:40:10 +0200

    Program to use any and all operators

---
+++ GeeksForGeeks/Operators/anyAllOperator.py
@@ -0,0 +1,19 @@
+# Usage of any operator in Python
+print(any([False, False, False, False]))
+
+# Usage of any operator in Python
+print(any([False, True, False, False]))
+
+# Usage of any operator in Python
+print(any([True, False, False, False]))
+
+print()
+
+# Usage of all operator in Python
+print(all([True, True, True, True]))
+
+# Usage of all operator in Python
+print(all([True, False, True, True]))
+
+# Usage of all operator in Python
+print(all([False, True, True, True]))
