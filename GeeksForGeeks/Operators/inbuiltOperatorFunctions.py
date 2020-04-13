505ade7ecdd48c3bf0db8bfd5dd4a3a403bcddd2
sumedh105 <sumedh105@gmail.com>
2020-04-13 11:51:10 +0200

    Progrm to implement the usage of inbuilt operators

---
+++ GeeksForGeeks/Operators/inbuiltOperatorFunctions.py
@@ -0,0 +1,43 @@
+import operator
+
+a = 5
+b = 10
+
+# Add two numbers using add() function
+print(operator.add(a, b))
+
+# Subtract two numbers using sub() function
+print(operator.sub(a, b))
+
+# Multiply two numbers using mul() function
+print(operator.mul(a, b))
+
+# Divide two numbers using trudiv() function
+print(operator.truediv(a, b))
+
+# Floor divide two numbers using floordiv() function
+print(operator.floordiv(a, b))
+
+# Use pow() function
+print(operator.pow(a, b))
+
+# Use mod() function
+print(operator.mod(a, b))
+
+# Use lt() function
+print(operator.lt(a, b))
+
+# Use le() function
+print(operator.le(a, b))
+
+# Use eq() function
+print(operator.eq(a, b))
+
+# Use gt() function
+print(operator.gt(a, b))
+
+# Use ge() function
+print(operator.ge(a, b))
+
+# Use ne(() function
+print(operator.ne(a, b))
