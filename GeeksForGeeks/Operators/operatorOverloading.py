8cb14874eaae27b29541799e84fcd2182e464593
sumedh105 <sumedh105@gmail.com>
2020-04-13 11:36:57 +0200

    Program to overload the operators in Python

---
+++ GeeksForGeeks/Operators/operatorOverloading.py
@@ -0,0 +1,16 @@
+# Using + operator to add two integers
+print(10 + 20)
+
+# Using + operator to concatenate two strings
+print("Sumedh " + "W. " + "Jambekar")
+
+# Using + operator to concatenate two lists
+ListOne = ["Sumedh ", "W. ", "Jambekar"]
+ListTwo = ["Kalaburagi", "Karnataka", "India"]
+print(ListOne + ListTwo)
+
+# Using + operator to concatenate two tuples
+TupleOne = ("Sumedh ")
+TupleTwo = ("Jambekar")
+print(TupleOne + TupleTwo)
+
