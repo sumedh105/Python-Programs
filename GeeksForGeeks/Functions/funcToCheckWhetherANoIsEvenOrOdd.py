e65b1f90f98c826414eb53b80399e881cc680cde
sumedh105 <sumedh105@gmail.com>
2020-04-13 13:19:04 +0200

    Program to check whether a given number is even or odd

---
+++ GeeksForGeeks/Functions/funcToCheckWhetherANoIsEvenOrOdd.py
@@ -0,0 +1,10 @@
+# Function to check whether a given number is even or odd
+def funEvenOdd(a):
+    if (a % 2) == 0:
+        print("The number is even")
+    elif (a % 2) == 1:
+        print("The number is odd")
+
+funEvenOdd(10)
+print()
+funEvenOdd(1)
