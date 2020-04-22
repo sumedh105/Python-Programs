1749e0d95c548bd9c0d3ba4ab100bb00666ef3c2
sumedh105 <sumedh105@gmail.com>
2020-04-22 23:44:28 +0200

    Program to implement voice.py

---
+++ randomPrograms/voice.py
@@ -0,0 +1,11 @@
+words = input("Say something\n").lower()
+
+if "hello" in words:
+    print("Hello to you too")
+elif "how are you" in words:
+    print("I am doing good, thank you!!")
+elif "bye see you again" in words:
+    print("Yeah, see you some time later")
+else:
+    print("Bye")
+    
